b8a154d5d67550464ea490239bee6e2dacf36cdc
Emmanuel Frecon <efrecon@gmail.com>
2020-04-18 22:48:17 +0200

    Adding option passing

--- libexec/steps/packages.sh
+++ libexec/steps/packages.sh
@@ -5,27 +5,47 @@
 # PACKAGE word twice because of naming conventions.
 PACKAGES_PACKAGES=${PACKAGES_PACKAGES:-}
 
+# Should we freshen up the system with all pending and existing upgrades.
+PACKAGES_FRESH=${PACKAGES_FRESH:-1}
+
 packages() {
     case "$1" in
+        "option")
+            shift;
+            while [ $# -gt 0 ]; do
+                case "$1" in
+                    --packages)
+                        PACKAGES_PACKAGES=$2; shift 2;;
+                    --fresh)
+                        PACKAGES_FRESH=$2; shift 2;;
+                    -*)
+                        yush_warn "Unknown option: $1 !";;
+                    *)
+                        break;;
+                esac
+            done
+            ;;
         "install")
             # Update package index in system
             primer_update
 
             # Freshen up system to latest
-            lsb_dist=$(primer_distribution)
-            case "$lsb_dist" in
-                ubuntu|*bian)
-                    $PRIMER_SUDO apt-get upgrade -y -q
-                    $PRIMER_SUDO apt-get dist-upgrade -y -q
-                    $PRIMER_SUDO apt-get autoremove -y -q
-                    ;;
-                alpine*)
-                    $PRIMER_SUDO apk upgrade
-                    ;;
-                clear*linux*)
-                    $PRIMER_SUDO swupd update
-                    ;;
-            esac
+            if yush_is_true "$PACKAGES_FRESH"; then
+                lsb_dist=$(primer_distribution)
+                case "$lsb_dist" in
+                    ubuntu|*bian)
+                        DEBIAN_FRONTEND=noninteractive $PRIMER_SUDO apt-get upgrade -y -q
+                        DEBIAN_FRONTEND=noninteractive $PRIMER_SUDO apt-get dist-upgrade -y -q
+                        DEBIAN_FRONTEND=noninteractive $PRIMER_SUDO apt-get autoremove -y -q
+                        ;;
+                    alpine*)
+                        $PRIMER_SUDO apk upgrade;;
+                    clear*linux*)
+                        $PRIMER_SUDO swupd update;;
+                    *)
+                        yush_warn "System update NYI for $lsb_dist";;
+                esac
+            fi
 
             # shellcheck disable=SC2086
             [ -n "$PACKAGES_PACKAGES" ] && primer_dependency "" $PACKAGES_PACKAGES
@@ -36,7 +56,7 @@ packages() {
                 case "$lsb_dist" in
                     ubuntu|*bian)
                         # shellcheck disable=SC2086
-                        $PRIMER_SUDO apt-get remove -y -q $PACKAGES_PACKAGES
+                        DEBIAN_FRONTEND=noninteractive $PRIMER_SUDO apt-get remove -y -q $PACKAGES_PACKAGES
                         ;;
                     alpine*)
                         # shellcheck disable=SC2086
@@ -46,6 +66,8 @@ packages() {
                         # shellcheck disable=SC2086
                         $PRIMER_SUDO swupd bundle-remove $PACKAGES_PACKAGES
                         ;;
+                    *)
+                        yush_warn "Package removal NYI for $lsb_dist";;
                 esac
             fi
             ;;
---
+++ libexec/steps/timezone.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env sh
+
+# The location to place the system at, e.g. Europe/Stockholm.
+TIMEZONE_LOCATION=${TIMEZONE_LOCATION:-}
+
+timezone() {
+    case "$1" in
+        "option")
+            shift;
+            while [ $# -gt 0 ]; do
+                case "$1" in
+                    --location)
+                        TIMEZONE_LOCATION=$2; shift 2;;
+                    -*)
+                        yush_warn "Unknown option: $1 !";;
+                    *)
+                        break;;
+                esac
+            done
+            ;;
+        "install")
+            lsb_dist=$(primer_distribution)
+            case "$lsb_dist" in
+                ubuntu|*bian)
+                    $PRIMER_SUDO ln -fs "/usr/share/zoneinfo/$LOCATION" /etc/localtime
+                    primer_dependency "" "tzdata"
+                    $PRIMER_SUDO dpkg-reconfigure --frontend noninteractive tzdata
+                    ;;
+                *)
+                    yush_warn "Timezone setting NYI for $lsb_dist";;
+            esac
+            ;;
+        "clean")
+            yush_warn "Timezone information cannot be removed"
+            ;;
+    esac
+}
