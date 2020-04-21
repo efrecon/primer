79bac505d4667878cc3ec1d0a4ab0da9e1c198e5
Emmanuel Frecon <efrecon@gmail.com>
2020-04-22 00:33:42 +0200

    Initial test suite

---
+++ spec/01-compose_spec.sh
@@ -0,0 +1,58 @@
+Describe "Compose"
+  Describe "Alpine"
+    Include ./spec/support/compose.sh
+
+    It "installed compose"
+      When call compose alpine
+      The status should be success
+      The output should include version
+    End
+
+    It "installed compose at precise version"
+      When call compose alpine 1.25.3
+      The status should be success
+      The output should include 1.25.3
+    End
+
+    It "installed compose with proper sha256 sum"
+      When call compose alpine 1.25.3 b3835d30f66bd3b926511974138923713a253d634315479b9aa3166c0050da98
+      The status should be success
+      The output should include 1.25.3
+    End
+
+    It "installed compose with improper sha256 sum"
+      When call compose alpine 1.25.3 b3835d30f66bd3b926511974138923713a253d634315479b9aa3166c00000000
+      The status should be failure
+      The output should include failed
+    End
+  End
+
+  Describe "Ubuntu"
+    Include ./spec/support/compose.sh
+
+    It "installed compose"
+      When call compose ubuntu
+      The status should be success
+      The output should include version
+    End
+
+    It "installed compose at precise version"
+      When call compose ubuntu 1.25.3
+      The status should be success
+      The output should include 1.25.3
+    End
+
+    It "installed compose with proper sha256 sum"
+      When call compose ubuntu 1.25.3 b3835d30f66bd3b926511974138923713a253d634315479b9aa3166c0050da98
+      The status should be success
+      The output should include 1.25.3
+    End
+
+    It "installed compose with improper sha256 sum"
+      When call compose ubuntu 1.25.3 b3835d30f66bd3b926511974138923713a253d634315479b9aa3166c00000000
+      The status should be success
+      The output should include 1.25.3
+    End
+  End
+
+End
