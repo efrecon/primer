82d9f9ea0733b8d0ef94467a0a73c20c89794f7f
Emmanuel Frecon <efrecon@gmail.com>
2020-04-22 00:33:25 +0200

    forever step to use from within tests

---
+++ libexec/steps/forever.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+
+# sleep time
+FOREVER_SLEEP=${FOREVER_SLEEP:-5}
+
+forever() {
+    case "$1" in
+        "option")
+            shift;
+            while [ $# -gt 0 ]; do
+                case "$1" in
+                    --sleep)
+                        FOREVER_SLEEP=$2;;
+                    -*)
+                        yush_warn "Unknown option: $1 !";;
+                    *)
+                        break;;
+                esac
+            done
+            ;;
+        "install")
+            yush_notice "Will sleep forever with at ${FOREVER_SLEEP}s intervals"
+            while true; do
+                sleep "$FOREVER_SLEEP"
+            done
+            ;;
+        "clean")
+            ;;
+    esac
+}
