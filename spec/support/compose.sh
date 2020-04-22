4694f096fe792cc088520ad6f2042f92b5cd6e62
Emmanuel Frecon <efrecon@gmail.com>
2020-04-23 00:42:12 +0200

    Single container tests

--- spec/support/compose.sh
+++ spec/support/compose.sh
@@ -4,24 +4,16 @@ compose() {
     img=${1:-alpine}
     version=${2:-}
     sum=${3:-}
-    container=$(  docker run \
-                    -d \
-                    -v "$(pwd):/primer:ro" \
-                    --entrypoint /primer/primer \
-                "$img" \
-                    -s "forever" 2>/dev/null)
-    docker exec "$container" \
-        /primer/primer \
-            -v debug \
-            -s compose \
+    docker run \
+            -i \
+            --rm \
+            -v "$(pwd):/primer:ro" \
+            --entrypoint /primer/primer \
+        "$img" \
+            -p /primer/libexec/steps:/primer/spec/support/steps \
+            -v error \
+            -s "compose runner" \
             --compose:version="$version" \
-            --compose:sha256="$sum" >/dev/null 2>&1
-    # Check if we can run --version and output the version line
-    retval=0
-    if ! docker exec -i "$container" docker-compose --version 2>/dev/null; then
-        retval=1
-    fi
-    # Clean away the container
-    docker container rm -fv "$container" >/dev/null 2>&1
-    return $retval
+            --compose:sha256="$sum" \
+            --runner:command="docker-compose --version"
 }
