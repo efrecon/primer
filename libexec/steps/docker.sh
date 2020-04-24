0854c9cd6c53bf195cd4c78b9793203b1ac3cabe
Emmanuel Frecon <efrecon@gmail.com>
2020-04-24 14:34:39 +0200

    More stringent security, interaction with users DB

--- libexec/steps/docker.sh
+++ libexec/steps/docker.sh
@@ -3,7 +3,15 @@
 # Short GPG signature for Docker Repo on debian derivatives
 DOCKER_APT_GPG=${DOCKER_APT_GPG:-0EBFCD88}
 
+# Space separated list of registry login information in the form
+# username:password@host
 DOCKER_REGISTRY=${DOCKER_REGISTRY:-}
+
+# Commit SHA sum contained in script, so we do not run something that has not
+# been verified.
+DOCKER_INSTALL_SHA256=${DOCKER_INSTALL_SHA256:-442e66405c304fa92af8aadaa1d9b31bf4b0ad94}
+
+DOCKER_ACCESS_ALL_USERS=${DOCKER_ACCESS_ALL_USERS:-1}
 
 # Where to get the docker installation script from
 DOCKER_GET_URL=https://get.docker.com/
@@ -15,9 +23,13 @@ docker() {
             while [ $# -gt 0 ]; do
                 case "$1" in
                     --registry)
-                        DOCKER_REGISTRY="$DOCKER_REGISTRY $2";;
+                        DOCKER_REGISTRY="$DOCKER_REGISTRY $2"; shift 2;;
+                    --sha256)
+                        DOCKER_INSTALL_SHA256="$2"; shift 2;;
+                    --access)
+                        DOCKER_ACCESS_ALL_USERS="$2"; shift 2;;
                     -*)
-                        yush_warn "Unknown option: $1 !";;
+                        yush_warn "Unknown option: $1 !"; shift 2;;
                     *)
                         break;;
                 esac
@@ -38,13 +50,23 @@ docker() {
                         primer_dependency curl
 
                         # Download the installation script from getdocker
-                        yush_info "Downloading and running Docker installation script from $(yush_yellow "$DOCKER_GET_URL"), this is a security risk"
+                        yush_info "Downloading and running Docker installation script from $(yush_yellow "$DOCKER_GET_URL")"
                         _get=$(mktemp)
                         curl -fsSL "$DOCKER_GET_URL" -o "$_get"
-                        sh "$_get"
+                        if [ -n "$DOCKER_INSTALL_SHA" ]; then
+                            if grep 'SCRIPT_COMMIT_SHA=' "$_get" | grep -q "$DOCKER_INSTALL_SHA256"; then
+                                yush_info "Verified Docker installation script, running it now"
+                                sh "$_get"
+                            else
+                                yush_warn "Commit SHA in script diffs from $DOCKER_INSTALL_SHA256. Maybe a new version? Verify the script and run again with --docker:sha256"
+                                yush_error "Unable to verify Docker installation script!!"
+                                return 1
+                            fi
+                        fi
 
                         # On debian we try to ensure that we have the proper
-                        # repository so we have some degree of security.
+                        # repository so we have some additional degree of
+                        # security.
                         case "$lsb_dist" in
                             ubuntu|*bian)
                                 yush_info "Verifying Docker GPG short key against: $(yush_green $DOCKER_APT_GPG)"
@@ -73,23 +95,53 @@ docker() {
                 primer_service enable docker
             fi
 
-            # Create group and make sure user is part of the group
-            yush_info "Adding $(id -un) to group docker"
-            _docker_group_create docker
-            _docker_group_membership "$(id -un)" docker
-
-            # Arrange for access to docker registries
-            for _registry in $DOCKER_REGISTRY; do
-                _user=$(printf %s\\n "$_registry" | sed -E -e 's/([^:]+)(:([^@]+))?@((([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])+)/\1/')
-                _pass=$(printf %s\\n "$_registry" | sed -E -e 's/([^:]+)(:([^@]+))?@((([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])+)/\3/')
-                _host=$(printf %s\\n "$_registry" | sed -E -e 's/([^:]+)(:([^@]+))?@((([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])+)/\4/')
-                if docker info 1>/dev/null; then
-                    yush_info "Logging in at $_host as $_user"
-                    printf %s\\n "$_pass" | docker login --password-stdin -u "$_user" "$_host"
+            # Perform user operations only if we have a docker client installed.
+            # Note that we cannot detect, nor call the docker client with the
+            # single string "docker" since it is the name of the function
+            # implemented by this module.
+            _docker_bin=$(which docker || true)
+            if [ -n "$_docker_bin" ]; then
+                # Create group and make sure user is part of the group
+                primer_group_create docker
+                primer_group_membership "$(id -un)" docker
+                if yush_is_true "$DOCKER_ACCESS_ALL_USERS"; then
+                    yush_debug "Adding all regular users on system to group docker"
+                    primer_users | grep -v "$(id -un)" | while IFS= read -r _username; do
+                        primer_group_membership "$_username" docker
+                    done
                 else
-                    yush_warn "Cannot login at remote registry $_registry, you might have to logout and login before running again"
+                    yush_info "Only $(id -un) has access to Docker"
                 fi
-            done
+
+                # Arrange for access to docker registries
+                _config=${DOCKER_CONFIG:-"$HOME/.docker"}/config.json
+                _prior_settings=0; [ -f "$_config" ] && _prior_settings=1
+                for _registry in $DOCKER_REGISTRY; do
+                    _user=$(printf %s\\n "$_registry" | sed -E -e 's/([^:]+)(:([^@]+))?@((([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])+)/\1/')
+                    _pass=$(printf %s\\n "$_registry" | sed -E -e 's/([^:]+)(:([^@]+))?@((([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])+)/\3/')
+                    _host=$(printf %s\\n "$_registry" | sed -E -e 's/([^:]+)(:([^@]+))?@((([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])+)/\4/')
+                    yush_info "Logging in at $_host as $_user"
+                    printf %s\\n "$_pass" | "$_docker_bin" login --password-stdin -u "$_user" "$_host"
+                done
+
+                if [ "$_prior_settings" = "0" ]; then
+                    primer_users | grep -v "$(id -un)" | while IFS= read -r _username; do
+                        _home=$(getent passwd | grep -E "^${_username}:" | cut -d ":" -f 6)
+                        if ! [ -f "$_home/.docker/config.json" ]; then
+                            $PRIMER_SUDO mkdir -p "$_home/.docker"
+                            $PRIMER_SUDO cp "$_config" "$_home/.docker/config.json"
+                            primer_ownership "$_home/.docker/config.json" --as "$_config" --user "$_username"
+                            yush_info "Local user $_username logged in at all Docker registries from above"
+                        else
+                            yush_warn "Skipped login for $_username, found existing settings at $_home/.docker/config.json"
+                        fi
+                    done
+                else
+                    yush_warn "Will not copy private Docker settings information to other users"
+                fi
+            else
+                yush_warn "No docker client installed!"
+            fi
 
             ;;
         "clean")
@@ -128,7 +180,7 @@ docker() {
                             yush_info "Removing docker from main repo list at /etc/apt/sources.list"
                             listtemp=$(mktemp)
                             grep -v "docker" /etc/apt/sources.list > "$listtemp"
-                            primer_root_ownership "$listtemp" /etc/apt/sources.list
+                            primer_ownership "$listtemp" --as /etc/apt/sources.list
                             $PRIMER_SUDO mv "$listtemp" /etc/apt/sources.list
                         fi
                         ;;
@@ -139,30 +191,4 @@ docker() {
             fi
             ;;
     esac
-}
-
-_docker_group_create() {
-    if ! getent group | grep -q "^$1:"; then
-        yush_info "Creating group: $1"
-        if [ -x "$(command -v "addgroup")" ]; then
-            $PRIMER_SUDO addgroup "$1"
-        elif [ -x "$(command -v "groupadd")" ]; then
-            $PRIMER_SUDO groupadd "$1"
-        fi
-    else
-        yush_debug "Group $1 already exists"
-    fi
-}
-
-_docker_group_membership() {
-    if id -Gn "$1" | grep -q "$2"; then
-        yush_debug "$1 already in group $2"
-    else
-        yush_info "Adding $1 to group $2"
-        if [ -x "$(command -v "adduser")" ]; then
-            $PRIMER_SUDO addgroup "$1" "$2"
-        elif [ -x "$(command -v "usermod")" ]; then
-            $PRIMER_SUDO usermod -a -G "$2" "$1"
-        fi
-    fi
 }
