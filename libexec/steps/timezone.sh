25780e8bde890aee8689ca979eccbbb3eb2bacd3
Emmanuel Frecon <efrecon@gmail.com>
2020-05-10 23:26:54 +0200

    force removal current timezone

--- libexec/steps/timezone.sh
+++ libexec/steps/timezone.sh
@@ -23,12 +23,14 @@ primer_step_timezone() {
                 lsb_dist=$(primer_os_distribution)
                 case "$lsb_dist" in
                     *buntu)
-                        $PRIMER_OS_SUDO ln -fs "/usr/share/zoneinfo/$PRIMER_STEP_TIMEZONE_LOCATION" /etc/localtime
+                        [ -f /etc/localtime ] && $PRIMER_OS_SUDO rm /etc/localtime
+                        $PRIMER_OS_SUDO ln -s "/usr/share/zoneinfo/$PRIMER_STEP_TIMEZONE_LOCATION" /etc/localtime
                         primer_os_dependency "" "tzdata"
                         $PRIMER_OS_SUDO dpkg-reconfigure --frontend noninteractive tzdata
                         ;;
                     *bian)
-                        $PRIMER_OS_SUDO ln -fs "/usr/share/zoneinfo/$PRIMER_STEP_TIMEZONE_LOCATION" /etc/localtime
+                        [ -f /etc/localtime ] && $PRIMER_OS_SUDO rm /etc/localtime
+                        $PRIMER_OS_SUDO ln -s "/usr/share/zoneinfo/$PRIMER_STEP_TIMEZONE_LOCATION" /etc/localtime
                         primer_os_dependency "" "tzdata"
                         $PRIMER_OS_SUDO dpkg-reconfigure --frontend noninteractive tzdata
                         ;;
