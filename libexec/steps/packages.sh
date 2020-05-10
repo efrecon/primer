dad56bffb2de4e3ebea6c41f6aeb6659f65c0baa
Emmanuel Frecon <efrecon@gmail.com>
2020-05-10 23:52:54 +0200

    proper var name

--- libexec/steps/packages.sh
+++ libexec/steps/packages.sh
@@ -3,7 +3,7 @@
 # List of packages to install. This is obviously limited by the fact that
 # packages are not called the same in all distributions. The variable has the
 # PACKAGE word twice because of naming conventions.
-PRIMER_STEP_PACKAGES_PRIMER_STEP_PACKAGES=${PRIMER_STEP_PACKAGES_PRIMER_STEP_PACKAGES:-}
+PRIMER_STEP_PACKAGES_PACKAGES=${PRIMER_STEP_PACKAGES_PACKAGES:-}
 
 # Should we freshen up the system with all pending and existing upgrades.
 PRIMER_STEP_PACKAGES_FRESH=${PRIMER_STEP_PACKAGES_FRESH:-1}
@@ -15,7 +15,7 @@ primer_step_packages() {
             while [ $# -gt 0 ]; do
                 case "$1" in
                     --packages)
-                        PRIMER_STEP_PACKAGES_PRIMER_STEP_PACKAGES=$2; shift 2;;
+                        PRIMER_STEP_PACKAGES_PACKAGES=$2; shift 2;;
                     --fresh)
                         PRIMER_STEP_PACKAGES_FRESH=$2; shift 2;;
                     -*)
@@ -35,15 +35,15 @@ primer_step_packages() {
                 primer_os_upgrade
             fi
 
-            if [ -n "$PRIMER_STEP_PACKAGES_PRIMER_STEP_PACKAGES" ]; then
+            if [ -n "$PRIMER_STEP_PACKAGES_PACKAGES" ]; then
                 # shellcheck disable=SC2086
-                primer_os_packages add $PRIMER_STEP_PACKAGES_PRIMER_STEP_PACKAGES
+                primer_os_packages add $PRIMER_STEP_PACKAGES_PACKAGES
             fi
             ;;
         "clean")
-            if [ -n "$PRIMER_STEP_PACKAGES_PRIMER_STEP_PACKAGES" ]; then
+            if [ -n "$PRIMER_STEP_PACKAGES_PACKAGES" ]; then
                 # shellcheck disable=SC2086
-                primer_os_packages del $PRIMER_STEP_PACKAGES_PRIMER_STEP_PACKAGES
+                primer_os_packages del $PRIMER_STEP_PACKAGES_PACKAGES
             fi
             ;;
     esac
