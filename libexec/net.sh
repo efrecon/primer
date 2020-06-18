89727c107f1fa806c9dc1e641b477cdb3e09d06d
Emmanuel Frecon <efrecon@gmail.com>
2020-06-18 14:28:43 +0000

    Add network module

---
+++ libexec/net.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+primer_net_interfaces() {
+    ip addr list |
+        grep -E -e '^[[:digit:]]{1,}:[[:space:]]*' |
+        sed -E 's|^[[:digit:]]{1,}:[[:space:]]*([^:]*):[[:space:]]*.*|\1|' |
+        sed -E 's/([^@]*)(@.*)?/\1/'
+}
+
+primer_net_macaddr() {
+    ip addr show "$1" |
+        grep -E '^[[:space:]]*link' |
+        grep -Eo '[0-9a-fA-F]{2}:[0-9a-fA-F]{2}:[0-9a-fA-F]{2}:[0-9a-fA-F]{2}:[0-9a-fA-F]{2}:[0-9a-fA-F]{2}' |
+        head -n 1 |
+        tr '[:upper:]' '[:lower:]'
+}
