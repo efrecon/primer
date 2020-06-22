d0df8471e9a79cab5c89ebb8a25a770fc2b2e15c
Emmanuel Frecon <efrecon@gmail.com>
2020-06-22 15:39:30 +0200

    Add URL encoding

--- libexec/net.sh
+++ libexec/net.sh
@@ -15,6 +15,25 @@ primer_net_macaddr() {
         tr '[:upper:]' '[:lower:]'
 }
 
+# shellcheck disable=SC2120
 primer_net_urldec() {
-    printf '%b\n' "$(sed -E -e 's/\+/ /g' -e 's/%([0-9a-fA-F]{2})/\\x\1/g')"
+    if [ "$#" -eq "0" ]; then
+        printf '%b\n' "$(sed -E -e 's/\+/ /g' -e 's/%([0-9a-fA-F]{2})/\\x\1/g')"
+    else
+        printf %s\\n "$1" | primer_net_urldec
+    fi
+}
+
+primer_net_urlenc() {
+    _encoded=
+    for _pos in $(seq 1 "${#1}"); do
+        _c=$(printf %s\\n "$1" | cut -c "$_pos")
+        case "$_c" in
+            [_.~a-zA-Z0-9-]) _encoded="${_encoded}${_c}" ;;
+            # The single quote before $_c below converts $_c to its numeric
+            # value: http://pubs.opengroup.org/onlinepubs/9699919799/utilities/printf.html#tag_20_94_13
+            *)               _encoded="${_encoded}$(printf '%%%02x' "'$_c")";;
+        esac
+    done
+    printf %s\\n "$_encoded"
 }
